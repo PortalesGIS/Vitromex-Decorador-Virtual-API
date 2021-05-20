58c2005c68ac769f4815d45db33a2f998204a774
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-05-20 17:22:42 -0500

    validate request whit middlewares

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,27 @@
 const {response } = require('express') ;
+const bcryptjs = require('bcryptjs');
 const User =require('../models/user');
+
 
 const userPost = async  (req,res = response) => {
 
+    // errores del middlewares
+    // 
+
     const {name,email,password,lastName} = req.body;
-    const user = new User(req.body) 
+    const user = new User({name,email,password,lastName}) 
+    // 
+    // verificar si email existe
+    const emailExist = await User.findOne({email:email});
+    if(emailExist){
+        return res.status(400).json({
+            msg:"El email ya esta registrado"
+        })
+    }
+    // encriptadr password
+    const salt = bcryptjs.genSaltSync();
+    user.password = bcryptjs.hashSync(password,salt)
+    // 
     try {
         await user.save();        
     } catch (error) {
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,20 @@
 
 const {Router} = require('express');
+const { check } = require('express-validator');
 const { userGet, userPost, userDelete } = require('../controllers/user');
-
+const {validateCampos} = require('../middlewares/validateCampos');
 
 const router = Router();
 
 router.get('/',userGet)
 
-router.post('/',userPost)
+router.post('/',[
+    check("email", "El correo no es valido").isEmail(),
+    check("name", "El nombre es requerido").not().isEmpty(),
+    check("password", "la contraseña es requerida y mayor a 6 caracteres ").isLength({min:6}),
+    check("lastName", "El apellido es requerido").not().isEmpty(),
+    validateCampos
+],userPost)
 // 
 router.delete('/',userDelete)
 
