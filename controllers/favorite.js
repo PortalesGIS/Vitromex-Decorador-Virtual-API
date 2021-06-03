308c003ff631ea6d8ce3a02177d884f62e80d94c
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-06-03 01:16:59 -0500

    feat: remove tofavorites endPoint

--- controllers/favorite.js
+++ controllers/favorite.js
@@ -26,6 +26,31 @@ const addPointFavorite = async(req,res=response)=>{
     })
 }
 
+const removePointFavorite = async (req,res=response)=>{
+    const {userId, productId} = req.body;
+
+    const state = await User.findById(userId)
+    if(!state.favorites.find((x)=>x===productId)){
+        return res.status(400).json({
+            ok:false,
+            msg:"el usuario NO lo tiene en favoritos"
+        })
+    }
+    
+
+    const fav = await Favorite.findById(productId);
+    await  fav.updateOne({total:fav.total-1})
+
+    const user = await User.findById(userId)
+    const {favorites} = user;
+    favorites.remove(productId)
+    await user.updateOne({favorites})
+    res.json({
+        ok:true
+    })
+}
+
 module.exports ={
-    addPointFavorite
+    addPointFavorite,
+    removePointFavorite
 }
--- routes/favorite.js
+++ routes/favorite.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { addPointFavorite } = require("../controllers/favorite");
+const { addPointFavorite, removePointFavorite } = require("../controllers/favorite");
 const { existFavoriteId,exitUserById } = require("../helpers/db-validators");
 const { validateCampos } = require("../middlewares/validateCampos");
 
@@ -13,6 +13,14 @@ router.post('/add/',[
     check("productId").custom(existFavoriteId),
     validateCampos
 ],addPointFavorite)
+
+router.post('/remove/',[
+    check("userId","No es un ID valido").isMongoId(),
+    check("productId","No es un ID valido").isMongoId(),
+    check("userId").custom(exitUserById),
+    check("productId").custom(existFavoriteId),
+    validateCampos
+],removePointFavorite)
 
 
 module.exports = router;
