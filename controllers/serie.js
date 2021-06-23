0d463a0cba49fc364241b9926fbcf860c464fbbb
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-06-23 13:23:00 -0500

    feat: add endpoint to serires datecreated

--- controllers/serie.js
+++ controllers/serie.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const Serie = require("../models/serie");
 
 
+// este NO regresa el campo "dateCreated"
 const getAllSeries =async (req,res = response) => {
     const serie =await Serie.find();
     const total = await Serie.count();
@@ -10,7 +11,28 @@ const getAllSeries =async (req,res = response) => {
     })
 }
 
+// este regresa el campo "dateCreated"
+const getAllSeriesCMS =async (req,res = response) => {
+    const ie =await Serie.find();
+    const serie = []
+    ie.forEach(elm=>{
+        serie.push( {
+            name:elm.name,               
+            img:elm.img,
+            _id:elm._id,
+            dateCreated:elm.dateCreated,
+            render:elm.render
+         } )
+     })
+     console.log(serie)
+    const total = await Serie.countDocuments();
+    res.json({total,
+       serie
+    })
+}
+
 
 module.exports={
     getAllSeries,
+    getAllSeriesCMS
 }
--- routes/series.js
+++ routes/series.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
-const { getAllSeries } = require("../controllers/serie");
+const { getAllSeries, getAllSeriesCMS } = require("../controllers/serie");
 
 const router = Router();
 
 router.get("/",
 getAllSeries
+);
+router.get("/cms",
+getAllSeriesCMS
 );
 
 module.exports = router;
